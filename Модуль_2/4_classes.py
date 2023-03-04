b4aa7ce7b449fa0d32f6ce73caaa87e2567f3c44
Anton <smirnovanton90@gmail.com>
2023-03-04 15:08:54 +0300

    Added new modules and lessons

---
+++ Модуль_2/4_classes.py
@@ -0,0 +1,11 @@
+class User:
+    pass
+
+peter = User()
+peter.name = "Peter Robertson"
+
+julia = User()
+julia.name = "Julia Donaldson"
+
+print(peter.name)
+print(julia.name)
---
+++ Модуль_2/6_methods.py
@@ -0,0 +1,50 @@
+class Product:
+    def __init__(self, name, category, quantity_in_stock):
+        self.name = name
+        self.category = category
+        self.quantity_in_stock = quantity_in_stock
+
+    def is_available(self):
+        return True if self.quantity_in_stock > 0 else False
+
+eggs = Product("eggs", "food", 5)
+print(eggs.is_available())
+
+
+class Event:
+    def __init__(self, timestamp=0, event_type="", session_id=""):
+        self.timestamp = timestamp
+        self.type = event_type
+        self.session_id = session_id
+    
+    def init_from_dict(self, event_dict):
+        self.timestamp = event_dict.get("timestamp")
+        self.type = event_dict.get("type")
+        self.session_id = event_dict.get("session_id")
+
+
+
+events = [
+    {
+     "timestamp": 1554583508000,
+     "type": "itemViewEvent",
+     "session_id": "0:NynteeXG:MYlskrqZbcmXNSFEJaZIsNVGeDLLpmct",
+    },
+    {
+     "timestamp": 1555296337000,
+     "type": "itemViewEvent",
+     "session_id": "0:NynteeXG:MYlskrqZbcmXNSFEJaZIsNVGeDLLpmct",
+    },
+    {
+     "timestamp": 1549461608000,
+     "type": "itemBuyEvent",
+     "session_id": "0:NynteeXG:MYlskrqZbcmXNSFEJaZIsNVGeDLLpmct",
+    },
+]
+
+
+for event in events:
+    event_obj = Event()
+    event_obj.init_from_dict(event)
+    print(event_obj.timestamp)
+
